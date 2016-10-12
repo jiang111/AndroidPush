fb1b8c4ffaa48d6cb1e67d3db68612434fd1e276
jyuesong <jyuesong@gmail.com>
2016-10-12 11:04:02 +0800

    add huawei  resource

--- push/src/main/java/com/jiang/android/push/utils/RomUtil.java
+++ push/src/main/java/com/jiang/android/push/utils/RomUtil.java
@@ -11,7 +11,7 @@ import java.lang.reflect.Method;
  */
 
 public class RomUtil {
-    private static Target mTarget = null;
+    private static Target mTarget = Target.EMUI;
     private static final String KEY_EMUI_VERSION_CODE = "ro.build.version.emui";
     private static final String KEY_MIUI_VERSION_CODE = "ro.miui.ui.version.code";
     private static final String KEY_MIUI_VERSION_NAME = "ro.miui.ui.version.name";
